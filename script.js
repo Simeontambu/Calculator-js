00b657f7ce00e2fff7d952d256e2221179bc23ee
simeontambu <simeontambu@gmail.com>
2023-08-11 12:15:10 +0100

    Selection of all buttons and operations

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-{ calculate } import './calculator';
+// { calculate } import './calculator';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+
+// selection of all buttons and operations of our calculator
+const displ = document.querySelector("#input");
+const previousOperand = querySelector('#calcul')
+const buttons = document.querySelectorAll("button");
+const reset = document.querySelector('#reset')
+const clear = document.querySelector('#clear')
+const equals = document.querySelector('#equals')
