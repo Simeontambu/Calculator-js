7d26d6616822941ee49c71548acfa2329b802b2d
simeontambu <simeontambu@gmail.com>
2023-08-14 11:39:45 +0100

    display of button values

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-// { calculate } import './calculator';
+import { calculate } from './calculator.js';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
 // selection of all buttons and operations of our calculator
-const displ = document.querySelector("#input");
-const previousOperand = querySelector('#calcul')
-const buttons = document.querySelectorAll("button");
+const currentOperand = document.querySelector("#input");
+const previousOperand = document.querySelector('#calcul')
+const buttons = document.querySelectorAll('.digit');
+const operationButtons = document.querySelectorAll('#divideby, #times')
 const reset = document.querySelector('#reset')
 const clear = document.querySelector('#clear')
 const equals = document.querySelector('#equals')
+
+// display of button values
+buttons.forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    previousOperand.insertAdjacentHTML('beforeend', button.textContent)
+    currentOperand.value = button.textContent
+  })
+})
+operationButtons.forEach((arithmeticbuttons)=>{
+    arithmeticbuttons.addEventListener('click', ()=>{
+        console.log(arithmeticbuttons)
+    })
+})
+
+
+
