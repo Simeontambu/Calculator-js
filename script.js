29815a6932c0d2cdf21599244ef6b7e30dc3d0f7
simeontambu <simeontambu@gmail.com>
2023-08-15 11:28:22 +0100

    Add method calculatepercentage

--- script.js
+++ script.js
@@ -1,166 +1,178 @@
-import { calculate } from './calculator.js';// TODO: Faire la manipulation du DOM dans ce fichier
+import { calculate } from "./calculator.js"; // TODO: Faire la manipulation du DOM dans ce fichier
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.previousOperandTextElement = previousOperandTextElement
-    this.currentOperandTextElement = currentOperandTextElement
-    this.clear()
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
   }
 
   clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
   }
 
   delete() {
-    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
   appendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) return
-    this.currentOperand = this.currentOperand.toString() + number.toString()
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
   chooseOperation(operation) {
-    if (this.currentOperand === '') return
-    if (this.previousOperand !== '') {
-      this.compute()
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
     }
-    this.operation = operation
-    this.previousOperand = this.currentOperand
-    this.currentOperand = ''
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
   }
-
-  compute() {
-    let computation
-    const prev = parseFloat(this.previousOperand)
-    const current = parseFloat(this.currentOperand)
-    if (isNaN(prev) || isNaN(current)) return
-    switch (this.operation) {
-      case '+':
-        computation = prev + current
-        break
-      case '-':
-        computation = prev - current
-        break
-      case '*':
-        computation = prev * current
-        break
-      case '÷':
-        computation = prev / current
-        break
-      default:
-        return
+  calculatepercentage(){
+    if(this.currentOperand !== ""){
+      this.currentOperand = this.currentOperand / 100;
     }
-    this.currentOperand = computation
-    this.operation = undefined
-    this.previousOperand = ''
+  }
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "x":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
   }
 
   getDisplayNumber(number) {
-    const stringNumber = number.toString()
-    const integerDigits = parseFloat(stringNumber.split('.')[0])
-    const decimalDigits = stringNumber.split('.')[1]
-    let integerDisplay
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
     if (isNaN(integerDigits)) {
-      integerDisplay = ''
+      integerDisplay = "";
     } else {
-      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0
+      });
     }
     if (decimalDigits != null) {
-      return `${integerDisplay}.${decimalDigits}`
+      return `${integerDisplay}.${decimalDigits}`;
     } else {
-      return integerDisplay
+      return integerDisplay;
     }
   }
   updateDisplay() {
-    this.currentOperandTextElement.value =
-      this.getDisplayNumber(this.currentOperand)
+    this.currentOperandTextElement.value = this.getDisplayNumber(
+      this.currentOperand
+    );
     if (this.operation != null) {
-      this.previousOperandTextElement.innerText =
-        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation} ${this.currentOperand}`;
     } else {
-      this.previousOperandTextElement.innerText = ''
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
 
-
-const numberButtons = document.querySelectorAll('.digit');
-const operationButtons = document.querySelectorAll('#divideby, #times, #plus,#minus')
-const equalsButton = document.querySelector('#equals')
-const deleteButton = document.querySelector('#clear')
-const allClearButton = document.querySelector('#reset')
-const previousOperandTextElement = document.querySelector('#calcul')
+const numberButtons = document.querySelectorAll(".digit,.dot");
+const operationButtons = document.querySelectorAll(
+  "#divideby,#times,#plus,#minus"
+);
+const percentagebutton = document.querySelector("#percentage");
+const equalsButton = document.querySelector("#equals");
+const deleteButton = document.querySelector("#clear");
+const allClearButton = document.querySelector("#reset");
+const previousOperandTextElement = document.querySelector("#calcul");
 const currentOperandTextElement = document.querySelector("#input");
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
 
-numberButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.appendNumber(button.innerText)
-    calculator.updateDisplay()
-  })
-})
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-operationButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.chooseOperation(button.innerText)
-    calculator.updateDisplay()
-  })
-})
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-equalsButton.addEventListener('click', button => {
-  calculator.compute()
-  calculator.updateDisplay()
-})
+equalsButton.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+percentagebutton.addEventListener("click", () => {
+calculator.calculatepercentage()
+calculator.updateDisplay();
+});
+allClearButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
 
-allClearButton.addEventListener('click', button => {
-  calculator.clear()
-  calculator.updateDisplay()
-})
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
 
-deleteButton.addEventListener('click', button => {
-  calculator.delete()
-  calculator.updateDisplay()
-})
-
-document.addEventListener('keydown', function (event) {
+document.addEventListener("keydown", function (event) {
   let patternForNumbers = /[0-9]/g;
-  let patternForOperators = /[+\-*\/]/g
+  let patternForOperators = /[+\-*\/]/g;
   if (event.key.match(patternForNumbers)) {
     event.preventDefault();
-    calculator.appendNumber(event.key)
-    calculator.updateDisplay()
+    calculator.appendNumber(event.key);
+    calculator.updateDisplay();
   }
-  if (event.key === '.') {
+  if (event.key === ".") {
     event.preventDefault();
-    calculator.appendNumber(event.key)
-    calculator.updateDisplay()
+    calculator.appendNumber(event.key);
+    calculator.updateDisplay();
   }
   if (event.key.match(patternForOperators)) {
     event.preventDefault();
-    calculator.chooseOperation(event.key)
-    calculator.updateDisplay()
+    calculator.chooseOperation(event.key);
+    calculator.updateDisplay();
   }
-  if (event.key === 'Enter' || event.key === '=') {
+  if (event.key === "Enter" || event.key === "=") {
     event.preventDefault();
-    calculator.compute()
-    calculator.updateDisplay()
+    calculator.compute();
+    calculator.updateDisplay();
   }
 
   if (event.key === "Backspace") {
     event.preventDefault();
-    calculator.delete()
-    calculator.updateDisplay()
+    calculator.delete();
+    calculator.updateDisplay();
   }
-  if (event.key == 'Delete') {
+  if (event.key == "Delete") {
     event.preventDefault();
-    calculator.clear()
-    calculator.updateDisplay()
+    calculator.clear();
+    calculator.updateDisplay();
   }
-
 });
-
-
-
